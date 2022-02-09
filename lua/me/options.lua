a58b6bd7bd8cfd70d947cd17a6d60073abb6e83d
Yazan Alshrouf <yazo_sh@yahoo.com>
2022-02-09 18:54:16 +0200

    added options

---
+++ lua/me/options.lua
@@ -0,0 +1,26 @@
+-- Neovim options
+
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.showtabline = 2
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+vim.opt.numberwidth = 4
+vim.opt.cursorline = true
+vim.opt.undofile = true
+vim.opt.termguicolors = true
+vim.opt.splitright = true
+vim.opt.splitbelow =  true
+vim.opt.smartindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.pumheight = 10
+vim.opt.mouse = "a"
+vim.opt.hlsearch = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.conceallevel = 0
+vim.opt.completeopt = {"menuone", "noselect"}
+vim.opt.cmdheight = 2
