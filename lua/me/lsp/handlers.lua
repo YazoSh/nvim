a69391fe30757af8b7b28cd79e912eb46b4287ed
Yazan Alshrouf <yazanalshrouf@gmail.com>
2022-02-24 14:38:22 +0200

    better now

--- lua/me/lsp/handlers.lua
+++ lua/me/lsp/handlers.lua
@@ -85,9 +85,8 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name ~= "null-ls" then
+	if client.name == "tsserver" then
 		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
