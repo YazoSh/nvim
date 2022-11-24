094e5452f8d57a87cc99ad613cbb10110de0e76f
Yazan Alshrouf <yazanalshrouf@gmail.com>
2022-11-25 00:30:20 +0300

    Updated Config

--- lua/me/lsp/handlers.lua
+++ lua/me/lsp/handlers.lua
@@ -95,7 +95,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if status_ok then
-	M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+	M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 else
 	print("faild to load completion for lsp")
 	M.capabilities = false
